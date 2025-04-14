7d667216c2f42a5aa4ce36600ad3cbcc2c7c00ce
Lina <linate.pa@gmail.com>
2025-04-14 11:12:16 +0300

    car

--- auto.js
+++ auto.js
@@ -4,4 +4,13 @@
 
 import {Car} from'./js/Car.js';
 
-const volvo=new Car();
+
+
+const volvo=new Car('Volvo S40','red',55);
+const zapas=new Car('Zapas','white',35);
+console.log(volvo.intro());
+console.log(zapas.intro());
+
+zapas.refill(5)
+console.log(zapas);
+console.log(zapas.refill(-5));
--- js/Car.js
+++ js/Car.js
@@ -1,6 +1,43 @@
 
 //produktas vienas is daliu
+/* NORIMA PASIEKTI:
 
+paskaiciuoti:
+METODAI
+    1)intro()-apibudina masinos modeli ir spalva
+    2)drive() - nurodzius norima nuvaziuoti atstuma yra paskaiciuojamos degalu sanaudos,
+    jei nepakaknka degalu , tai ribiniu atvetu tiesiog sustoja ir masina issijungia.
+    3)engineOn()-ijungia varikli, jei yra kuro ir jei yra isjungtas
+    4)engineOff()- isungia varikli, jei yra ijungtas
+    5)refill()-uzpilti degalu, bet ne daugiau nei telpa ir jei variklis yra isjungtas
+    */
 export class Car {
-
+    constructor(model,color, tankMax){
+        this.model=model;
+        this.color=color;
+        this.tankMax=tankMax;
+        this.tank=0;
+        this.engineIsOn=false;
+    }
+    intro(){
+        return`This is ${this.color} ${this.model}.`
+    }
+    refill(volume){
+        if(this.engineIsOn){
+        return 'Error: to refil a tank. you must turn off engine first.';
+    }
+        if(typeof volume!== "number"|| !isFinite(volume)){
+            return'Error: Only numbers allowed.';
+        }
+        if (volume<0){
+        return`Refill with ${volume} litres is not allowed`
+    }
+   
+        if(this.tank>=volume){
+        this.tank+=volume;
+    }
+        else {
+            this.tank=this.tankMax;
+        }
+    }
 }
